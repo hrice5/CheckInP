3f93a459b5fa1ffea39e3f01d74edc02ab7c8d29
hrice5 <angel_of_darkness170@yahoo.com>
2016-12-11 16:04:35 -0600

    Add files via upload

--- src/pages/registration/registration.ts
+++ src/pages/registration/registration.ts
@@ -2,14 +2,16 @@ import { Component } from '@angular/core';
 
 import { NavController } from 'ionic-angular';
 
+
 @Component({
   selector: 'page-registration',
-  templateUrl: 'registration.html'
+  templateUrl: 'registration.html',
 })
 export class RegistrationPage {
 
   constructor(public navCtrl: NavController) {
 
   }
+
 
 }
