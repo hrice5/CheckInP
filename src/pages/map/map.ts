495752d250b1bfcb1fed3dd7586a18c08a4b2ca8
hrice5 <angel_of_darkness170@yahoo.com>
2016-12-11 16:04:10 -0600

    Add files via upload

--- src/pages/map/map.ts
+++ src/pages/map/map.ts
@@ -2,14 +2,16 @@ import { Component } from '@angular/core';
 
 import { NavController } from 'ionic-angular';
 
+
+
+
 @Component({
   selector: 'page-map',
   templateUrl: 'map.html'
 })
 export class MapPage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController) {}
 
-  }
 
 }
