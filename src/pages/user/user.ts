ea44ec40c0cb68c2bdb59de27efb775230055068
hrice5 <angel_of_darkness170@yahoo.com>
2016-12-11 16:05:11 -0600

    Add files via upload

--- src/pages/user/user.ts
+++ src/pages/user/user.ts
@@ -3,14 +3,21 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
 @Component({
+
+  
   selector: 'page-user',
-  templateUrl: 'user.html'
+  templateUrl: 'user.html',
+
 })
 export class UserPage {
   myDate: Date[] = [];
-
+  
   constructor(public navCtrl: NavController) {
-
   }
+
+setTime(index: number){
+  this.myDate[index] = new Date();
+}
+
 
 }
